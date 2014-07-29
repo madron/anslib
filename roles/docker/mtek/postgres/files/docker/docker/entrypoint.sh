78e848c71b324223088317b83c16a1221273e0e0
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-07-29 20:22:32 +0200

    added rendering to /mtek/postgres config files

--- roles/docker/mtek/postgres/files/docker/docker/entrypoint.sh
+++ roles/docker/mtek/postgres/files/docker/docker/entrypoint.sh
@@ -2,25 +2,28 @@
 set -e
 
 export VERSION=9.1
-export PGDATA=/var/lib/postgresql/$VERSION
-export POSTGRES=/usr/lib/postgresql/$VERSION/bin/postgres
-export CONFIG_DIR=/etc/postgresql/$VERSION/main
+export PGBIN=/usr/lib/postgresql/${VERSION}/bin
+export PGDATA=/data
 
 
 if [[ "$1" = "postgres" ]]; then
-    chown -R postgres "$PGDATA"
+    chown -R postgres "${PGDATA}"
 
-    if [ -z "$(ls -A "$PGDATA")" ]; then
-        /usr/bin/pg_createcluster $VERSION main
-        cp /docker/pg_hba.conf $CONFIG_DIR/
-        cp /docker/pg_ident.conf $CONFIG_DIR/
-        cp /docker/postgresql.conf $CONFIG_DIR/
-
-        #sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
-        # { echo; echo 'host all all 0.0.0.0/0 trust'; } >> "$PGDATA"/pg_hba.conf
+    # Create cluster if it doesn't exist
+    if [ -z "$(ls -A "${PGDATA}")" ]; then
+        sudo -i -u postgres ${PGBIN}/initdb --pgdata=${PGDATA} --encoding=UTF8
     fi
 
-    sudo -i -u postgres $POSTGRES -D $PGDATA/main -c config_file=$CONFIG_DIR/postgresql.conf
+    # Config files
+    mkdir -p "${PGDATA}"/etc
+    # pg_hba.conf will be created only if it doesn't exist
+    if [ ! -f "${PGDATA}/etc/pg_hba.conf" ]; then
+        /docker/render.py --template /docker/pg_hba.conf --outfile ${PGDATA}/etc/pg_hba.conf
+    fi
+    # postgresql.conf is always overwritten
+    /docker/render.py --template /docker/postgresql.conf --outfile ${PGDATA}/etc/postgresql.conf
+
+    sudo -i -u postgres ${PGBIN}/postgres -D ${PGDATA} -c config_file=${PGDATA}/etc/postgresql.conf
 
 else
     exec "$@"
