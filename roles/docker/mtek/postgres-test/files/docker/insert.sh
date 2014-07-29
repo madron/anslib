6da60b91b721fda0468ed0516b7b21fc2ba65867
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-07-29 16:35:48 +0200

    limited script output in docker/mtek/postgres-test

--- roles/docker/mtek/postgres-test/files/docker/insert.sh
+++ roles/docker/mtek/postgres-test/files/docker/insert.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-    psql -h postgres -U postgres -c "INSERT INTO log DEFAULT VALUES RETURNING id, insert_time;" test
+    psql -h postgres -U postgres -c "INSERT INTO log DEFAULT VALUES RETURNING id, insert_time;" test | grep "\."
     sleep 1
 done
--- roles/docker/mtek/postgres-test/files/docker/select.sh
+++ roles/docker/mtek/postgres-test/files/docker/select.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-    psql -h postgres -U postgres -c "select id, insert_time from log order by id desc limit 1;" test
+    psql -h postgres -U postgres -c "select id, insert_time from log order by id desc limit 1;" test | grep "\."
     sleep 1
 done
