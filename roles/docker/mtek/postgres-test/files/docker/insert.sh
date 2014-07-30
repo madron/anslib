cf2c578de78a82c6250294eaaa9c2e1e590a062d
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-07-30 16:12:29 +0200

    added data column to test table in mtek/postgres-test role

--- roles/docker/mtek/postgres-test/files/docker/insert.sh
+++ roles/docker/mtek/postgres-test/files/docker/insert.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    export SIZE=1
+else
+    export SIZE=$1
+fi
+
+
+if [ -z "$2" ]; then
+    export DELAY=1
+else
+    export DELAY=$2
+fi
+
+
 while true
 do
-    psql -h postgres -U postgres -c "INSERT INTO log DEFAULT VALUES RETURNING id, insert_time;" test | grep "\."
-    sleep 1
+    psql -h postgres -U postgres -c "INSERT INTO log (data) VALUES (repeat('1234567890ABCDEF', 64 * ${SIZE} )) RETURNING id, insert_time, char_length(data);" test | grep "\."
+    sleep ${DELAY}
 done
