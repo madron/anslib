95e132e263644a2c20c815baa006424e9fb339d9
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-08-24 16:49:11 +0200

    added millproject file creation to madron/pcb2gcode role

--- roles/docker/madron/pcb2gcode/files/docker/docker/entrypoint.sh
+++ roles/docker/madron/pcb2gcode/files/docker/docker/entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 if [[ "$1" = "bash" ]]; then
     exec "$@"
+elif [[ "$1" = "millproject" ]]; then
+    echo ${*:2}
+    /docker/millproject.py ${*:2}
 else
     cd /data
     /usr/bin/pcb2gcode $@
