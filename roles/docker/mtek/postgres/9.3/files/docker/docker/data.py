d2ceeea76fd6fd9b7f1d9c8a37f59c9fa7daa43b
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-10-09 09:37:24 +0200

    refactored mtek/postgres/9.3 data.py

--- roles/docker/mtek/postgres/9.3/files/docker/docker/data.py
+++ roles/docker/mtek/postgres/9.3/files/docker/docker/data.py
@@ -1,1 +1,6 @@
-data = dict()
+import os
+
+data = dict(
+    WAL_KEEP_SEGMENTS=os.getenv('WAL_KEEP_SEGMENTS', 0),
+    MASTER_SERVER = os.getenv('WAL_KEEP_SEGMENTS', None),
+)
--- roles/docker/mtek/postgres/9.3/files/docker/docker/entrypoint.sh
+++ roles/docker/mtek/postgres/9.3/files/docker/docker/entrypoint.sh
@@ -4,14 +4,7 @@ set -e
 export VERSION=9.3
 export PGBIN=/usr/lib/postgresql/${VERSION}/bin
 export PGDATA=/data
-export DATA_FILE=/docker/data.py
-export DATA_DEFAULT=/docker/default_data.py
 
-
-echo "data=dict()" >  $DATA_FILE
-echo "data['WAL_KEEP_SEGMENTS']='$WAL_KEEP_SEGMENTS'" >>  $DATA_FILE
-echo "data['MASTER_SERVER']='$MASTER_SERVER'" >>  $DATA_FILE
-cat $DATA_DEFAULT >> $DATA_FILE
 
 chown -R postgres.postgres /var/run/postgresql
 chmod 775 /var/run/postgresql
@@ -44,15 +37,22 @@ elif [[ "$1" = "syncuser" ]]; then
     psql -h postgres -U postgres -c "CREATE USER syncuser REPLICATION LOGIN CONNECTION LIMIT 1 ENCRYPTED PASSWORD '$2';"
 
 elif [[ "$1" = "backup" ]]; then
-    if [ -d "/backup/pg_basebackup" ]; then
-        # Abort if directory exists
-        echo "Directory /backup/pg_basebackup already exists"
+    if [ -f "/backup/base.tar.gz" ]; then
+        # Abort if file exists
+        echo "File /backup/base.tar.gz already exists"
         exit 1
     fi
+    if [ -d "/backup/pg_basebackup" ]; then
+        # Abort if working directory exists
+        echo "Working directory /backup/pg_basebackup already exists: maybe another backup is running"
+        exit 1
+    fi
+    rm -rf /backup/pg_basebackup
     mkdir /backup/pg_basebackup
     chown postgres /backup/pg_basebackup
     sudo -i -u postgres ${PGBIN}/pg_basebackup --pgdata=/backup/pg_basebackup --xlog --format=tar --progress --verbose
-    gzip --rsyncable /backup/pg_basebackup/base.tar
+    gzip --rsyncable --stdout /backup/pg_basebackup/base.tar > /backup/base.tar.gz
+    # rm -rf /backup/pg_basebackup
 
 elif [[ "$1" = "restore" ]]; then
     cd /
