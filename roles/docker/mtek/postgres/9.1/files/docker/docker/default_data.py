878ca05a6359fa1cbfe4d823ada8fc12b86167d8
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-11-18 16:03:03 +0100

    refactored postgres/9.1 role

--- roles/docker/mtek/postgres/9.1/files/docker/docker/default_data.py
+++
@@ -1,4 +0,0 @@
-if not data['WAL_KEEP_SEGMENTS']:
-    data.pop('WAL_KEEP_SEGMENTS')
-if not data['MASTER_SERVER']:
-    data.pop('MASTER_SERVER')
--- roles/docker/mtek/postgres/9.1/files/docker/docker/entrypoint.sh
+++ roles/docker/mtek/postgres/9.1/files/docker/docker/entrypoint.sh
@@ -3,62 +3,90 @@ set -e
 
 export VERSION=9.1
 export PGBIN=/usr/lib/postgresql/${VERSION}/bin
-export PGDATA=/data
-export DATA_FILE=/docker/data.py
-export DATA_DEFAULT=/docker/default_data.py
 
 
-echo "data=dict()" >  $DATA_FILE
-echo "data['WAL_KEEP_SEGMENTS']='$WAL_KEEP_SEGMENTS'" >>  $DATA_FILE
-echo "data['MASTER_SERVER']='$MASTER_SERVER'" >>  $DATA_FILE
-cat $DATA_DEFAULT >> $DATA_FILE
+chown -R postgres.postgres /var/run/postgresql
+chmod 775 /var/run/postgresql
 
 if [[ "$1" = "postgres" ]]; then
-    chown -R postgres "${PGDATA}"
+    chown -R postgres "/data"
 
     # Create cluster if it doesn't exist
-    if [ -z "$(ls -A "${PGDATA}")" ]; then
-        sudo -i -u postgres ${PGBIN}/initdb --pgdata=${PGDATA} --encoding=UTF8
+    if [ -z "$(ls -A "/data")" ]; then
+        sudo -i -u postgres ${PGBIN}/initdb --pgdata=/data --encoding=UTF8
+        # Config files
+        rm /data/pg_ident.conf
+        /docker/render.py --template /docker/conf/postgresql.conf --outfile /data/postgresql.conf
+        /docker/render.py --template /docker/conf/pg_hba.conf --outfile /data/pg_hba.conf
+        # Init sql
+        if [ -f "/docker/sql/init.sql" ]; then
+            echo "Loading init.sql"
+            sudo -i -u postgres pg_ctl start -w -D /data --silent -o "-c listen_addresses=''"
+            sudo -i -u postgres psql -f /docker/sql/init.sql
+            sudo -i -u postgres pg_ctl stop -w -D /data --silent
+        fi
     fi
 
-    # Config files
-    mkdir -p "${PGDATA}"/etc
-    # pg_hba.conf will be created only if it doesn't exist
-    if [ ! -f "${PGDATA}/etc/pg_hba.conf" ]; then
-        /docker/render.py --template /docker/pg_hba.conf --outfile ${PGDATA}/etc/pg_hba.conf
-    fi
-    # postgresql.conf is always overwritten
-    /docker/render.py --template /docker/postgresql.conf --outfile ${PGDATA}/etc/postgresql.conf
+    # pg_hba.conf  is always overwritten
+    /docker/render.py --template /docker/conf/pg_hba.conf --outfile /data/pg_hba.conf
+
     # recovery.conf is always overwritten
-    rm -f ${PGDATA}/recovery.conf
-    if [ ! "${MASTER_SERVER}" == "" ]; then
-        /docker/render.py --template /docker/recovery.conf --outfile ${PGDATA}/recovery.conf
+    rm -f /data/recovery.conf
+    if [ ! "${master_server}" == "" ]; then
+        /docker/render.py --template /docker/conf/recovery.conf --outfile /data/recovery.conf
     fi
 
-    sudo -i -u postgres ${PGBIN}/postgres -D ${PGDATA} -c config_file=${PGDATA}/etc/postgresql.conf
+    # Start postgres
+    echo "Starting postgres"
+    exec sudo -i -u postgres exec ${PGBIN}/postgres -D /data -c config_file=/data/postgresql.conf
+
+elif [[ "$1" = "pg_hba" ]]; then
+    /docker/render.py --template /docker/conf/pg_hba.conf --outfile /data/pg_hba.conf
+    exec sudo -i -u postgres psql -c "SELECT pg_reload_conf();" > /dev/null
+
+elif [[ "$1" = "psql" ]]; then
+    exec sudo -i -u postgres "$@"
 
 elif [[ "$1" = "syncuser" ]]; then
-    psql -h postgres -U postgres -c "CREATE USER syncuser REPLICATION LOGIN CONNECTION LIMIT 1 ENCRYPTED PASSWORD '$2';"
+    exec sudo -i -u postgres psql -c "CREATE USER syncuser REPLICATION LOGIN CONNECTION LIMIT 1 ENCRYPTED PASSWORD '$2';"
 
 elif [[ "$1" = "backup" ]]; then
-    if [ -d "/backup/data" ]; then
-        # Abort if directory exists
-        echo "Directory /backup/data already exists"
+    if [[ "$2" = "--overwrite" ]]; then
+        rm -rf /backup/pg_basebackup
+        rm -f /backup/base.tar.gz
+    fi
+    if [ -f "/backup/base.tar.gz" ]; then
+        # Abort if file exists
+        echo "File /backup/base.tar.gz already exists."
         exit 1
     fi
-    export FORMAT=plain
-    export GZIP_OPTION=""
-    if [[ "$2" = "tar" ]]; then
-        export FORMAT=tar
-        export GZIP_OPTION="--gzip"
+    if [ -d "/backup/pg_basebackup" ]; then
+        # Abort if working directory exists
+        echo "Working directory /backup/pg_basebackup already exists: maybe another backup is running."
+        exit 1
     fi
-    ${PGBIN}/pg_basebackup --pgdata=/backup/data --xlog --format=${FORMAT} ${GZIP_OPTION} --progress --verbose --host=postgres --username=syncuser
+    rm -rf /backup/pg_basebackup
+    mkdir /backup/pg_basebackup
+    chown postgres /backup/pg_basebackup
+    sudo -i -u postgres ${PGBIN}/pg_basebackup --pgdata=/backup/pg_basebackup --xlog --format=tar --progress --verbose
+    gzip --rsyncable --stdout /backup/pg_basebackup/base.tar > /backup/base.tar.gz
+    rm -rf /backup/pg_basebackup
 
 elif [[ "$1" = "restore" ]]; then
-    # sudo -i -u postgres ${PGBIN}/postgres -D ${PGDATA} -c config_file=${PGDATA}/etc/postgresql.conf
-    cd /
-    tar xvfz /backup/db_data.tgz
-
+    # Check if backup exists
+    if [ ! -f "/backup/base.tar.gz" ]; then
+        echo "File /backup/base.tar.gz doesn't exist."
+        exit 1
+    fi
+    # Check if postgres is running
+    if [ -S "/var/run/postgresql/.s.PGSQL.5432" ]; then
+        echo "Found postgres running: aborting."
+        exit 1
+    fi
+    # Remove old data and restore
+    cd /data
+    rm -rf *
+    tar xfz /backup/base.tar.gz
 else
     exec "$@"
 fi
