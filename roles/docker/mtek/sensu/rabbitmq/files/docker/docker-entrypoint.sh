908643c6a7faae8b0aa3db8fb750a14dbda10e3f
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2015-02-08 21:57:26 +0100

    removed mtek/sensu/rabbitmq role

--- roles/docker/mtek/sensu/rabbitmq/files/docker/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$1" = 'rabbitmq-server' ]; then
-    chown -R rabbitmq /var/lib/rabbitmq
-    sed -i "s/REPLACEME/$RABBITMQ_PASSWORD/g" /etc/rabbitmq/rabbitmq.config
-fi
-
-exec "$@"
