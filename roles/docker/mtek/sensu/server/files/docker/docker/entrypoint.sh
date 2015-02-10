deca08e62519712a4ab1d3e8311c4456dc3eb926
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2015-02-10 16:15:33 +0100

    added conf.d directory to mtek/sensu/server entrypoint.sh

--- roles/docker/mtek/sensu/server/files/docker/docker/entrypoint.sh
+++ roles/docker/mtek/sensu/server/files/docker/docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = 'sensu-server' ]; then
     /docker/render.py --template /docker/conf/server.json --outfile /etc/sensu/server.json
-    exec /opt/sensu/bin/sensu-server --config /etc/sensu/server.json
+    exec /opt/sensu/bin/sensu-server --config /etc/sensu/server.json --config_dir /etc/sensu/conf.d
 fi
 
 exec "$@"
